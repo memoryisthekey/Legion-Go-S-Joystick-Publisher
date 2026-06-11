dc98b5b3971a3b4442de6ab3772bc2eaf658f023
Legion Go S <username@users.noreply.gitlab.com>
2026-06-11 14:09:11 +0200

    Change topic type to stamped

--- legion_joystick/legion_joystick.py
+++ legion_joystick/legion_joystick.py
@@ -3,14 +3,14 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import TwistStamped
 
 
 class LegionJoystick(Node):
     def __init__(self):
         super().__init__('legion_joystick')
 
-        self.cmd_pub = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.cmd_pub = self.create_publisher(TwistStamped, '/cmd_vel', 10)
         self.joy_sub = self.create_subscription(Joy, '/joy', self.joy_callback, 10)
 
         self.timer = self.create_timer(0.05, self.publish_cmd)  # 20 Hz
@@ -35,10 +35,13 @@ class LegionJoystick(Node):
         self.latest_joy = msg
 
     def publish_cmd(self):
-        twist = Twist()
+        twist_stamped = TwistStamped()
+
+        twist_stamped.header.stamp = self.get_clock().now().to_msg()
+        twist_stamped.header.frame_id = 'base_link'  # or 'cmd_vel_link', depending on your robot setup
 
         if self.latest_joy is None:
-            self.cmd_pub.publish(twist)
+            self.cmd_pub.publish(twist_stamped)
             return
 
         buttons = self.latest_joy.buttons
@@ -48,18 +51,18 @@ class LegionJoystick(Node):
         fast_pressed = buttons[self.right_bumper_button] == 1
 
         if slow_pressed:
-            twist.linear.x = self.slow_linear * axes[self.linear_axis]
-            twist.angular.z = self.slow_angular * axes[self.angular_axis]
+            twist_stamped.twist.linear.x = self.slow_linear * axes[self.linear_axis]
+            twist_stamped.twist.angular.z = self.slow_angular * axes[self.angular_axis]
 
         elif fast_pressed:
-            twist.linear.x = self.fast_linear * axes[self.linear_axis]
-            twist.angular.z = self.fast_angular * axes[self.angular_axis]
+            twist_stamped.twist.linear.x = self.fast_linear * axes[self.linear_axis]
+            twist_stamped.twist.angular.z = self.fast_angular * axes[self.angular_axis]
 
         else:
-            twist.linear.x = 0.0
-            twist.angular.z = 0.0
+            twist_stamped.twist.linear.x = 0.0
+            twist_stamped.twist.angular.z = 0.0
 
-        self.cmd_pub.publish(twist)
+        self.cmd_pub.publish(twist_stamped)
 
 
 def main():
